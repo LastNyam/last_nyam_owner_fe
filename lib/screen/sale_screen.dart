ca27682aeec9a3974d2b966942908ff36c062e08
oko_jin <dswvgw6412@naver.com>
2024-11-29 02:18:19 +0900

    feat: 냠냠판매 화면 초기 세팅

---
+++ lib/screen/sale_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class SaleScreen extends StatelessWidget {
+  const SaleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('냠냠판매 화면'),
+    );
+  }
+}
