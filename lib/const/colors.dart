580027d405ab97924755257ff25a5cdc4d2668a4
oko_jin <dswvgw6412@naver.com>
2024-11-29 02:17:58 +0900

    feat: 컬러 팔레트 구현

---
+++ lib/const/colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+Map<int?, Color?> grey = {
+  100: Color(0xfff3f4f6),
+  200: Color(0xffe5e7eb),
+  300: Color(0xffd1d5db),
+  350: Color(0xff747b88),
+  400: Color(0xff9ca3af),
+  500: Color(0xff6b7280),
+  600: Color(0xff4b5563),
+  700: Color(0xff374151),
+  800: Color(0xff1f2937),
+  900: Color(0xff111827),
+  950: Color(0xff030712),
+};
+
+Map<String?, Color?> mannerTemperature = {
+  'primary': Color(0xff0000ff),
+  'secondary': Color(0xff00bfff),
+  'tertiary': Color(0xff7fff00),
+  'quaternary': Color(0xff32cd32),
+  'quinary': Color(0xffffa500),
+  'senary': Color(0xffffa500),
+  'septenary': Color(0xffff6347),
+  'octonary': Color(0xffff0000),
+};
+
+Map<String?, Color?> defaultColors = {
+  'green': Color(0xff417c4e),
+  'lightGreen': Color(0xffb9c6bc),
+  'pureWhite': Color(0xffffffff),
+  'white': Color(0xfff2f2f2),
+  'black': Color(0xff262626),
+  'brown': Color(0xff604c4d),
+};
